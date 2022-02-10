81f858b1a4ed25e250df576d102524140d26b4a8
Aaryan Neupane <sonofnaleen@gmail.com>
2022-02-11 00:29:04 +0545

    Dark mode created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parichaya_frontend/providers/documents.dart';
+import 'package:parichaya_frontend/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'screens/buttom_navigation_base.dart';
 import 'screens/add_document.dart';
@@ -8,7 +9,10 @@ import 'screens/edit_document.dart';
 import 'screens/full_screen_image.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider<ThemeProvider>(
+    create: (_) => ThemeProvider(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,29 +22,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => Documents(),
-      child: MaterialApp(
-        title: 'Parichaya',
-        // TODO: Customize theme
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'OpenSans',
-          // fontFamily: 'Quicksand',
-          // splashFactory: InkRipple.splashFactory,
-        ),
-        debugShowCheckedModeBanner: false,
-        routes: {
-          '/': (ctx) => const ButtomNavigationBase(),
-          AddDocuments.routeName: (ctx) => const AddDocuments(),
-          DocumentDetails.routeName: (ctx) => const DocumentDetails(),
-          EditDocument.routeName: (ctx) => const EditDocument(),
-          FullScreenImage.routeName: (ctx) => const FullScreenImage(),
-        },
-        onUnknownRoute: (settings) {
-          return MaterialPageRoute(
-            builder: (ctx) => const ButtomNavigationBase(),
-          );
-        },
-      ),
+      child: Consumer<ThemeProvider>(builder: (context, provider, child) {
+        return MaterialApp(
+          title: 'Parichaya',
+
+          // TODO: Customize theme
+          //themeMode: ThemeMode.system,
+          // theme: ThemeData(
+          //   primarySwatch: Colors.blue,
+          //   fontFamily: 'OpenSans',
+          //   // brightness: Brightness.dark,
+          //   // fontFamily: 'Quicksand',
+          //   // splashFactory: InkRipple.splashFactory,
+          // ),
+
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode: provider.themeMode,
+
+          //lightTheme: MyThemes.lightTheme,
+
+          debugShowCheckedModeBanner: false,
+          routes: {
+            '/': (ctx) => const ButtomNavigationBase(),
+            AddDocuments.routeName: (ctx) => const AddDocuments(),
+            DocumentDetails.routeName: (ctx) => const DocumentDetails(),
+            EditDocument.routeName: (ctx) => const EditDocument(),
+            FullScreenImage.routeName: (ctx) => const FullScreenImage(),
+          },
+          onUnknownRoute: (settings) {
+            return MaterialPageRoute(
+              builder: (ctx) => const ButtomNavigationBase(),
+            );
+          },
+        );
+      }),
     );
   }
 }
---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool currentTheme = false;
+
+  ThemeMode get themeMode {
+    if (currentTheme == false)
+      return ThemeMode.light;
+    else if (currentTheme == true)
+      return ThemeMode.dark;
+    else
+      return ThemeMode.system;
+  }
+
+  changeTheme(bool theme) async {
+    AnimatedSwitcher(duration: const Duration(milliseconds: 0));
+    //final SharedPreferences _prefs = await SharedPreferences.getInstance();
+    //await _prefs.setBool('theme', theme);
+    currentTheme = theme;
+    notifyListeners();
+  }
+
+  //initialize() async {
+  //final SharedPreferences _prefs = await SharedPreferences.getInstance();
+  //currentTheme = _prefs.getBool('theme') ?? false;
+  //notifyListeners();
+  // }
+}
